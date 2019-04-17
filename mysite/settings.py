d6f9c69c994abd68d9d8503fb1f6154797b54e9e
yuanping24 <34534454+yuanping24@users.noreply.github.com>
2019-04-17 13:41:58 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
